6fde3156703b77b371e505ec999217773a418e02
unknown <jose@pippa.com.br>
2020-12-06 16:08:02 -0300

    4. Faq Section

--- src/pages/about-us/about-us.component.jsx
+++ src/pages/about-us/about-us.component.jsx
@@ -1,10 +1,12 @@
 import AboutSection from "../../components/about-section/about-section.component";
+import FaqSection from "../../components/faq-section/faq-section.component";
 import ServicesSection from "../../components/services-section/services-section.component";
 const AboutUs = () => {
   return (
     <>
       <AboutSection />
       <ServicesSection />
+      <FaqSection />
     </>
   );
 };
